0e2f0b80a39d3da9ad076054b098b38b90e6e32a
mcorcias <mosescorcias@gmail.com>
2019-10-17 11:59:39 +0300

    handler files added

--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
-const fs = require("fs");
-const req = require("request");
-const { parse } = require('querystring');
+
+const handlers = require('./handlers');
+const { homeHandler,
+    fetchNames,
+    middlewareStaticFiles,
+    notFoundPage } = require("./handlers")
 
 //this module lets us construct the filepath
 const path = require("path");
@@ -11,83 +14,16 @@ const router = (request, response) => {
 
     if (request.method === "GET") {
         if (url === "/") {
-            fs.readFile(
-                path.join(__dirname, "..", "public", "index.html"),
-                (error, file) => {
-                    if (error) {
-                        console.log(error);
-                        response.writeHead(500, { "Content-Type": "text/html" });
-                        response.end(
-                            "<h1>Sorry, we have had a problem with the server </h1>"
-                        );
-                    } else {
-                        response.writeHead(200, { "Content-Type": "text/html" });
-                        response.end(file);
-                    }
-                }
-            );
+            homeHandler(request, response);
+            response.setHeader("Location", user);
         } else if (url === '/fetchNames') {
+            fetchNames(response);
 
-            var options = {
-                method: 'GET',
-                url: 'https://free-nba.p.rapidapi.com/players',
-                qs: { page: '0', per_page: '100' },
-
-
-                headers: {
-                    'x-rapidapi-host': 'free-nba.p.rapidapi.com',
-                    'x-rapidapi-key': 'd2f04481bfmsh7b77972de4ec2dfp1964bdjsn3338e26bee8f'
-                }
-
-
-
-
-            };
-
-            req(options, function (error, res, body) {
-                if (error) throw new Error(error);
-
-                response.end(body);
-            });
         }
         else if (url.indexOf("/public/") !== -1) {
-            const extension = url.split(".")[1];
-            const extensionType = {
-                html: "text/html",
-                css: "text/css",
-                js: "application/javascript",
-                ico: "image/x-icon"
-            };
-            const filePath = path.join(__dirname, "..", url);
-            fs.readFile(filePath, (error, file) => {
-                if (error) {
-                    response.writeHead(500, { "Content-Type": "text/html" });
-                    response.end("<h1>Sorry, we have had a problem with the server </h1>");
-                } else {
-                    response.writeHead(200, { "Content-Type": extensionType[extension] });
-                    response.end(file);
-                }
-            });
+            middlewareStaticFiles(response, url);
         } else {
-            response.writeHead(404);
-            response.end("404 not found");
-        }
-    } else if (request.method === "POST") {
-        if (url === "/") {
-            let body = '';
-            request.on('data', chunk => {
-                body += chunk.toString(); // convert Buffer to string
-            });
-            request.on('end', () => {
-                const fullName = parse(body).player;
-                const firstName = fullName.split(" ")[0];
-                const lastName = fullName.split(" ")[1];
-                response.end(
-                    `<h1>The NBA player which you choosed is: ${fullName} <h1>
-                    <iframe width="400" height="400" src="https://nba-players.herokuapp.com/players/${firstName.toLowerCase()}"></iframe>
-                    `
-                );
-            });
+            notFoundPage(response);
         }
     }
 
